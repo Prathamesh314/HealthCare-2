f388a50d0fb4d4492fffbc80f889e623976160b4
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2024-01-11 21:15:37 +0530

    integrated chatbot in website

--- health-care/src/components/Feed.tsx
+++ health-care/src/components/Feed.tsx
@@ -13,25 +13,30 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/components/ui/avatar"
+import Image from 'next/image'
 
-interface Promptoutput{
+type Promptoutput = {
   name: string,
   message: string,
 }
 
 const Feed = () => {
   const [userInput, setUserInput] = useState('');
+  let [results, setresults] = useState<Promptoutput[]>([])
 
-  const results: Promptoutput[] | undefined = []
 
   const handleInputChange = (e: any) => {
     setUserInput(e.target.value);
   };
 
+  const handleClear = ()=>{
+    setresults([])
+  }
+
   const handleSubmit = async(e: any) => {
     e.preventDefault();
     let msg1 = {name: "User", message: userInput}
-    results.push(msg1)
+    results = [...results, msg1]
     await fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
             body: JSON.stringify({ message: userInput }),
@@ -42,9 +47,14 @@ const Feed = () => {
           }).then(r => r.json())
           .then(r => {
             let msg2 = { name: "Prathamesh", message: r.message };
-            results.push(msg2);
+            console.log(msg2)
+            setresults([...results, msg2])
         })
+      
+        setUserInput('')
   };
+
+  console.log(results)
   
   return (
     <div className='flex w-full justify-betweem items-center gap-x-4'>
@@ -68,15 +78,13 @@ const Feed = () => {
               <h1>Ask your healthy doubts here...</h1>
             </SheetDescription>
             <hr className='mt-3 border-t-10 border-black' />
-            <div className='flex flex-col gap-y-4'>
-              <div className='h-[530px] border-1 border-black bg-gray-100' >
-              {/* <ul>
-                {results.map((result, index) => (
-                  <li key={index}>
-                    <strong>{result.name}:</strong> {result.message}
-                  </li>
+            <div className='flex flex-col gap-y-4 mt-1'>
+              <div className='h-[530px] gap-y-4 overflow-y-auto' >
+                {results?.map((result, index)=>(
+                  <div key={index} className='font-md text-black gapy-3 mb-2'>
+                    {result.name} : {result.message}
+                  </div>
                 ))}
-              </ul> */}
               </div>
               <div className='flex justify-center items-center w-full'>
                 <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center w-full'>
@@ -89,12 +97,21 @@ const Feed = () => {
                         type="text"
                         value={userInput}
                         onChange={handleInputChange}
-                        className='h-8 p-1 text-black font-semibold'
+                        className='h-8 p-1 text-black font-semibold border-black border-1'
                       />
                     </label>
                   </div>
                   <div className='mt-2'>
-                    <button type="submit" className='w-20 h-10 bg-blue-400 rounded-2xl border-2 border-blue-900 text-md font-mono text-black'>Submit</button>
+                    <div className='flex justify-center items-center gap-x-10'>
+                      <div>
+                        <button type="submit" className='w-20 h-10 bg-blue-400 rounded-2xl border-2 border-blue-900 text-md font-mono text-black' onClick={handleSubmit}>Submit</button>
+                      </div>
+                      <div>
+                        <button onClick={handleClear} className='w-20 h-10 bg-red-400 rounded-2xl border-2 border-red-900 text-md font-mono text-black'>
+                          Clear
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </form>
               </div>
